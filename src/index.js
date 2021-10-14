eb1f97ed3f67cafd5164010f3a7618d77d8bf622
Ziv5ha <89574596+Ziv5ha@users.noreply.github.com>
2021-10-15 00:16:43 +0300

    Error handles and specialty
    added error handlers and a function to calculate the highest stat of the pokemon

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@
 const searchBox = document.getElementById("search-box")
 const searchButton = document.getElementById("search-button")
 // Outputs
+const spriteElem = document.getElementById("pokemon-sprite")
 const idElem = document.getElementById("pokemon-id")
 const nameElem = document.getElementById("pokemon-name")
 const renderElem = document.getElementById("pokemon-render")
@@ -9,23 +10,25 @@ const speciesElem = document.getElementById("pokemon-species")
 const typeElem = document.getElementById("pokemon-type")
 const heightElem = document.getElementById("pokemon-height")
 const weightElem = document.getElementById("pokemon-weight")
+const specialtyElem = document.getElementById("pokemon-specialty")
 const familyTreeElem = document.getElementById("pokemon-family-tree")
-const descriptionElem = document.getElementById("pokemon-description")
 
 async function getPokemon(pokemonId){
     try{
         const pokemonPromise = await axios.get(`https://pokeapi.co/api/v2/pokemon/${pokemonId}/`)
         return pokemonPromise.data
     } catch (error){
+        handleError()
         throw error
-        // throw new Error `can't find Pokemon`
     }
     
 }
 async function displayPokemon(pokemonId){
     try{
+        removeError()
         deleteTypes()
         const pokemonData = await getPokemon(pokemonId)
+        spriteElem.src = pokemonData.sprites.front_default
         idElem.innerText = pokemonData.id
         nameElem.innerText = pokemonData.name
         renderElem.src = `https://img.pokemondb.net/sprites/home/normal/${pokemonData.name}.png`
@@ -33,9 +36,10 @@ async function displayPokemon(pokemonId){
         displayTypes(pokemonData.types)
         heightElem.innerText = pokemonData.height
         weightElem.innerText = pokemonData.weight
+        specialtyElem.innerText = calculateSpecialty(pokemonData.stats)
         console.log(pokemonData)
-    } catch {
-        
+    } catch (error){
+        throw `can't find Pokemon. ${error}`
     }
 }
 //pokemons can have multible types. this function handles those types
@@ -52,8 +56,40 @@ function deleteTypes(){
     const prevTypes = document.getElementsByClassName("type-box")
     if (prevTypes.length>0) typeElem.innerHTML = ""
 }
-displayPokemon(280)
+// displayPokemon(280)
 function searchPokemon(){
     displayPokemon(searchBox.value)
 }
 searchButton.addEventListener("click", searchPokemon)
+
+// Error handles
+function handleError(){
+    const errorElem = document.createElement("div")
+    errorElem.id = "error"
+    errorElem.innerText = `Error! can't find Pokemon`
+    document.body.appendChild(errorElem)
+}
+function removeError(){
+    const checkPrevError = document.getElementById("error")
+    if (checkPrevError !== null){
+        document.body.removeChild(checkPrevError)
+    }
+}
+// Speciality calculates the pokemon's speciality and returns it.
+// For example: alakazam have low defence and very high sp.attack so it's speciality is sp.attack.
+function calculateSpecialty(statsObj){
+    const sipmleStatsObj = {
+        HP: statsObj[0].base_stat,
+        ATK: statsObj[1].base_stat,
+        DEF: statsObj[2].base_stat,
+        SPATK: statsObj[3].base_stat,
+        SPDEF: statsObj[4].base_stat,
+        }
+    const statsArr = [sipmleStatsObj.HP, sipmleStatsObj.ATK, sipmleStatsObj.DEF, sipmleStatsObj.SPATK, sipmleStatsObj.SPDEF]
+    const highestStat = Math.max(...statsArr)
+    for (const [statName, statValue] of Object.entries(sipmleStatsObj)){
+        if (statValue === highestStat) return `The Pokemon's highest stat is ${statName}: ${statValue}`
+    }
+}
+
+displayPokemon(280)
